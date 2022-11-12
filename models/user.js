7417a572efd0d9555365d4440cf38ba729a5a088
Kyle Davis <kyle.davis@ucdenver.edu>
2022-11-11 19:54:36 -0700

    migrated new model and worked on assocations

--- models/user.js
+++ models/user.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Post, { foreignKey: 'userId' })
+      User.hasMany(models.Comment, { foreignKey: 'userId' })
     }
   }
   User.init(
